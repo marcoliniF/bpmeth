cd1c9ae7c43e3b5faeb6604593eb0d33255c9520
Francesco Emanuele Marcolini <fmarcoli@pcbe17097.dyndns.cern.ch>
2026-07-30 14:19:45 +0200

    created function to plot convergence for different h values

---
+++ preliminaryTrials/6.4plotconvergences.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+orders=np.linspace(2,10,9)
+rmsxh1=np.array([5.09329580e-07,7.28402546e-08, 8.84989448e-09, 6.49769258e-10, 8.41810736e-11, 5.33244687e-11, 3.07666867e-11, 2.86895711e-11,2.86432182e-11])
+rmsxh01=np.array
